332ba232d9b01247d0456977619db5fb01ccb6e4
Methode-virtuelle-pur <antoine.anquetil@ensg.eu>
2024-12-14 12:25:44 +0100

    Ajout du test lint et supression de la demo de test de github actions

--- tests.py
+++ tests.py
@@ -9,3 +9,11 @@ def test_program_builds():
     except subprocess.CalledProcessError:
         pytest.fail("Le programme n'a pas pu être exécuté correctement.")
 
+
+def test_code_quality():
+    """Vérifie la qualité du code avec flake8."""
+    try:
+        # Lancer flake8 sur le projet
+        subprocess.run(["flake8", "main.py"], check=True)
+    except subprocess.CalledProcessError as e:
+        pytest.fail(f"Linting échoué : {e}")
