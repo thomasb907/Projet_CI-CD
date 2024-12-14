0de4e96ed7bea328a863fc6d6381e06427c5329f
BoThoma <thomas.boutonne@ensg.eu>
2024-12-14 14:53:26 +0100

    Version intermediaire probleme connection le local

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import requests
+import fire
+
+def list_vehicule(address="http://localhost:8080/vehicles"):
+    try:
+        response = requests.get(address)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        return f"Erreur lors de la requête : {e}"
 
 if __name__ == '__main__':
-    print("Hello World")
+    print(list_vehicule())
